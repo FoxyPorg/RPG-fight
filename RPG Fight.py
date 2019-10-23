178d35ff9f453b4f3f040188fcced7676e67ce59
FoxyPorg <56735642+FoxyPorg@users.noreply.github.com>
2019-10-23 21:33:40 +0200

    Create RPG Fight.py

---
+++ RPG Fight.py
@@ -0,0 +1,39 @@
+print ('Hello Word')
+
+#Tour 1
+
+#
+
+## Initialisation ##
+# choix d'une capacitée
+# Enemy choisi capacitée aleatoire
+#la vittesse influe sur l'ordre d'apparition du text
+
+capacite=str(input("Les capacité:\n capacitée1 \n capacitée2 \n capacitée3 \n capacitée4 \n capacitée5 \n capacitée6:"))
+
+
+
+
+
+
+
+
+
+'''
+class Joueur: #Joueur1
+    def __init__(self)
+        self.PV = 11
+        self.Chance = 7
+        self.Force = 10
+        self.Vitesse = 16
+        self.Endurance = 10
+
+
+class Ennemy:
+    def __init__(self)
+        self.PV = 10
+        self.Chance = 10
+        self.Force = 10
+        self.Vitesse = 10
+        self.Endurance = 10
+'''
